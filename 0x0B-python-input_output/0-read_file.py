51500f7826ae45fb262ac6bd62739ba0f6fa51bd
resiato <resiatolaitete@gmail.com>
2021-10-12 11:33:57 -0700

    Update README.md

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,6 @@ Reads from a file and prints.
 def read_file(filename=""):
     """Reads from filename and prints
     its contents to stdout.
-
     Args:
         - filename: name of the file
     """
