1d56d0bded5594eaab65e76a6c4d5a3b45eb04e0
Resiato <resiatolaitete@gmail.com>
2022-02-01 12:16:53 +0000

    Removed comments

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-/* File System Object */
 const fs = require('fs');
 
-/* Read File */
 fs.readFile(process.argv[2], 'utf-8', bar)
 
-/*If an error exits, show it, otherwise display the file */
 function bar (err, data) {
 	if(err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(data));
+		console.log(data);
 	}
 };
