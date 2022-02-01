267d6bd0eedefa8ef0e6d25a9bd65b5468c2dc02
Resiato <resiatolaitete@gmail.com>
2022-02-01 12:26:12 +0000

    Removed unwanted semicolons and tab spaces

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', bar)
+fs.readFile(process.argv[2], 'utf-8', bar);
 
 function bar (err, data) {
-	if(err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
-};
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+}
