58fbe6340a7c06debebf5ca4418700248a725d4a
Resiato <resiatolaitete@gmail.com>
2022-02-02 19:17:32 +0000

    Modified function code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 
 try {
   fs.writeFile(process.argv[2], process.argv[3], 'utf-8', bar);
-}
 
 function bar (err, data) {
   if (err) {
     console.log(err);
   }
+}
 }
 
 catch (err) {
