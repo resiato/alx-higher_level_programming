c1318bea6bf6be527398677b4e4e560030680de0
Resiato <resiatolaitete@gmail.com>
2022-02-02 19:15:27 +0000

    Move function declaration to program root

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 
 try {
   fs.writeFile(process.argv[2], process.argv[3], 'utf-8', bar);
+}
 
 function bar (err, data) {
   if (err) {
     console.log(err);
   }
-};
 }
+
 catch (err) {
  console.log(err);
 }
