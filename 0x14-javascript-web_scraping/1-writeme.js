597c0cec37952243252492dc12004c1723093f19
Resiato <resiatolaitete@gmail.com>
2022-02-02 19:08:11 +0000

    Update README.md

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 try {
   fs.writeFile(process.argv[2], process.argv[3], 'utf-8', bar);
 
-  function bar (err, data) {
-    if (err) {
-     console.log(err);
-    }
+function bar (err, data) {
+  if (err) {
+    console.log(err);
   }
 }
+}
 catch (err) {
-  console.log(err);
+ console.log(err);
 }
